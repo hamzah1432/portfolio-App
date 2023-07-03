4e33188cde566461685213b812e0348c52e3a2f8
hamzah1432 <halmuhesn@gmail.com>
2023-07-03 07:52:00 +0300

    About done

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -1,14 +1,25 @@
 
 function About(props: { rotation: number }) {
+
+    function Hundred() {
+        if (props.rotation <= 40) return props.rotation / 50;
+        return 100
+    }
+
     return (
 
         <div className="About" >
-            <div className="heading">
+            <div className="heading" style={{ opacity:  Hundred() }}>
                 <h1>Education background</h1>
 
-                <p>Bachelor of Engineering in Mechanical Engineering, Mutah<br/>
-                    Graduation Year: Sep 2017 - Aug 2021<br/>
+                <p>Bachelor of Engineering in Mechanical<br /> Engineering, Mutah<br />
+                    Graduation Year: Sep 2017 - Aug 2021<br />
                     Grade: very good</p>
+
+                <p>Diploma in UI/UX Design, LTUC<br /> Expected Completion: Jan 2024</p>
+                <a>And more than 10 certificate of completion
+                    from</a>
+                    <img src="edu.png" style={{marginTop:70}}/>
             </div>
         </div>
     );
